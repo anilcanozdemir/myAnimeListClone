0f10e80d36437da5697baa86f4def0d2ba56d611
ANIL CAN ÖZDEMİR <anilcanozdemir1020@gmail.com>
2023-11-09 22:52:11 +0300

    Initial commit

---
+++ src/main/java/com/myanimelist/myAnimeListClone/MyAnimeListCloneApplication.java
@@ -0,0 +1,13 @@
+package com.myanimelist.myAnimeListClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAnimeListCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAnimeListCloneApplication.class, args);
+	}
+
+}
