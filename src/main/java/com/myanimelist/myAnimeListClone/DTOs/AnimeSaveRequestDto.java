072e0ca386a1542dbe564cf035c8b2ad21cfd9f7
ANIL CAN ÖZDEMİR <anilcanozdemir1020@gmail.com>
2023-12-01 17:28:32 +0300

    AnimeDTO s created.
    AnimeDTOları oluşturuldu.

---
+++ src/main/java/com/myanimelist/myAnimeListClone/DTOs/AnimeSaveRequestDto.java
@@ -0,0 +1,28 @@
+package com.myanimelist.myAnimeListClone.DTOs;
+
+import com.myanimelist.myAnimeListClone.Entity.Genre;
+import com.myanimelist.myAnimeListClone.Entity.Item.Anime;
+import com.myanimelist.myAnimeListClone.Entity.Item.Manga;
+import com.myanimelist.myAnimeListClone.Entity.Item.Review;
+import com.myanimelist.myAnimeListClone.Entity.ItemCharacter;
+import com.myanimelist.myAnimeListClone.Enums.AnimeType;
+import com.myanimelist.myAnimeListClone.Enums.Demographic;
+import com.myanimelist.myAnimeListClone.Enums.ItemStatus;
+import com.myanimelist.myAnimeListClone.Enums.Theme;
+import lombok.Value;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * DTO for {@link Anime}
+ */
+@Value
+public record AnimeSaveRequestDto(Integer episodeCount, ItemStatus itemStatus, Date startDate, Date finishDate,
+                                  Set<Genre> genreSet, Theme theme, Demographic demographic, String snopsis,
+                                  List<ItemCharacter> characters, List<Review> itemReviews, AnimeType animeType,
+                                  Integer durationByMinute, Manga adaptionManga, List<Anime> alternativeVersionAnime,
+                                  List<Anime> sideStoryAnimes, List<Anime> spinOfAnimes) implements Serializable {
+}
