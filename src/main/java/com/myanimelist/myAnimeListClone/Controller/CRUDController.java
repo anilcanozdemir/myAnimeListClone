f9344fecbd4b032dfd2e20e1705d6c7b4d53e2f2
ANIL CAN ÖZDEMİR <anilcanozdemir1020@gmail.com>
2023-12-01 17:28:00 +0300

    Base CRUDController and CRUDService written.
    Gerekli base classlar yazıldı bunlarla beraber bütün controller ve servicelerin sahip olması gereken methodlar interfacelere bağlanmış oldu.

---
+++ src/main/java/com/myanimelist/myAnimeListClone/Controller/CRUDController.java
@@ -0,0 +1,21 @@
+package com.myanimelist.myAnimeListClone.Controller;
+
+
+
+import com.myanimelist.myAnimeListClone.Core.Result.DataResult;
+import com.myanimelist.myAnimeListClone.Core.Result.Result;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface CRUDController<EntityResponseDto, EntitySaveRequestDto, EntityUpdateRequestDto> {
+    public ResponseEntity<Result> add(EntitySaveRequestDto entitySaveRequestDto);
+
+    public ResponseEntity<DataResult<List<EntityResponseDto>>> getAll();
+
+    public ResponseEntity<DataResult<EntityResponseDto>> getById(Long id);
+
+    public ResponseEntity<Result> updateById(EntityUpdateRequestDto entityUpdateRequestDto);
+
+    public ResponseEntity<DataResult<EntityResponseDto>> deleteById(Long id);
+}
--- src/main/java/com/myanimelist/myAnimeListClone/Service/Contracts/CRUDService.java
+++ src/main/java/com/myanimelist/myAnimeListClone/Service/Contracts/CRUDService.java
@@ -1,5 +1,18 @@
 package com.myanimelist.myAnimeListClone.Service.Contracts;
 
-public interface CRUDService<EntityAdd,Entity> {
 
+
+import java.util.List;
+
+public interface CRUDService<EntityResponseDto, EntitySaveRequestDto, EntityUpdateRequestDto> {
+
+    Result add(EntitySaveRequestDto entitySaveRequestDto);
+
+    DataResult<EntityResponseDto> deleteByid(Long id);
+
+    DataResult<List<EntityResponseDto>> getAll();
+
+    DataResult<EntityResponseDto> getById(Long id);
+
+    Result updateById(EntityUpdateRequestDto entityUpdateRequestDto);
 }
