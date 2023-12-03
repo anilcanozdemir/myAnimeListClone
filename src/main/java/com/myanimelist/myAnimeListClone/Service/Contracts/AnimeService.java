080c756a6391ee0e246f416b93d4d5723ba9d894
ANIL CAN ÖZDEMİR <anilcanozdemir1020@gmail.com>
2023-12-03 18:54:01 +0300

    Update AnimeService.java

--- src/main/java/com/myanimelist/myAnimeListClone/Service/Contracts/AnimeService.java
+++ src/main/java/com/myanimelist/myAnimeListClone/Service/Contracts/AnimeService.java
@@ -1,4 +1,8 @@
 package com.myanimelist.myAnimeListClone.Service.Contracts;
 
-public interface AnimeService extends CRUDService {
+import com.myanimelist.myAnimeListClone.DTOs.AnimeResponseDto;
+import com.myanimelist.myAnimeListClone.DTOs.AnimeSaveRequestDto;
+import com.myanimelist.myAnimeListClone.DTOs.AnimeUpdateRequestDto;
+
+public interface AnimeService extends CRUDService<AnimeResponseDto, AnimeSaveRequestDto, AnimeUpdateRequestDto> {
 }
