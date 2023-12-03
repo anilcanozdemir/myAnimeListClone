967f2db9e1efeec43460dc733a9389c4b0ab8108
ANIL CAN ÖZDEMİR <anilcanozdemir1020@gmail.com>
2023-12-03 18:58:17 +0300

    Create AnimeRepository.java

---
+++ src/main/java/com/myanimelist/myAnimeListClone/Repository/AnimeRepository.java
@@ -0,0 +1,7 @@
+package com.myanimelist.myAnimeListClone.Repository;
+
+import com.myanimelist.myAnimeListClone.Entity.Item.Anime;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnimeRepository extends JpaRepository<Anime,Long> {
+}
