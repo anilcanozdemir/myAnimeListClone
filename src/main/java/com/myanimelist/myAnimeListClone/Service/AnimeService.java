640e32cb90632736f9a51ad339a2e69ebe8406f9
ANIL CAN ÖZDEMİR <anilcanozdemir1020@gmail.com>
2023-12-03 18:53:44 +0300

    Delete AnimeService.java

--- src/main/java/com/myanimelist/myAnimeListClone/Service/AnimeService.java
+++
@@ -1,8 +0,0 @@
-package com.myanimelist.myAnimeListClone.Service;
-
-import com.myanimelist.myAnimeListClone.Service.Contracts.AnimeService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class AnimeManager implements AnimeService {
-}
